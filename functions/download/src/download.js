05a2fa837f3dfdf2e9e67cf9177b7a23d1475d53
Tim Cash <timcash@gmail.com>
2018-05-01 21:10:53 -0600

    formatting

--- functions/download/src/download.js
+++ functions/download/src/download.js
@@ -7,23 +7,20 @@ const download = async (req, res) => {
   const id = uuid.v4()
   console.log(`${id} starting`)
   const file = myBucket.file(`${id}.xml.gz`)
-  get(
-    "https://exchangefeeds.s3.amazonaws.com/9d2dcb702d7d6b801f34227c04c8bb23/feed.xml.gz",
-    function(err, res) {
-      if (err) {
-        console.error(`${id} ${err.toString()}`)
-      }
-      console.log(`${id} ${res.statusCode}`)
-      res
-        .pipe(file.createWriteStream())
-        .on("error", function(err) {
-          console.error(`${id} ${err.toString()}`)
-        })
-        .on("finish", function() {
-          console.log(`${id} complete`)
-        }) // `res` is a stream
+  get("git stat", function(err, res) {
+    if (err) {
+      console.error(`${id} ${err.toString()}`)
     }
-  )
+    console.log(`${id} ${res.statusCode}`)
+    res
+      .pipe(file.createWriteStream())
+      .on("error", function(err) {
+        console.error(`${id} ${err.toString()}`)
+      })
+      .on("finish", function() {
+        console.log(`${id} complete`)
+      }) // `res` is a stream
+  })
 
   res.send({ id, status: "downloading" })
 }
