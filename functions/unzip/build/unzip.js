2474994f6cc3ad5e736b167636f6f66bc7c90fca
Tim Cash <timcash@gmail.com>
2018-05-08 10:48:56 -0600

    added permissions to the build agent

--- functions/unzip/build/unzip.js
+++ functions/unzip/build/unzip.js
@@ -4,15 +4,10 @@ let unzip = (() => {
   var _ref = _asyncToGenerator(function* (req, res) {
     const id = uuid.v4();
     try {
-      const result = yield do_file_things(req);
-      res_ok(res, { id, function_name });
-      return success({
-        id,
-        function_name
-      });
+      const result = yield do_file_things(id, req);
+      return res_ok(res, { id, function_name });
     } catch (e) {
-      res_err(res, { id, function_name, error: e.toString() });
-      return failure(e.toString());
+      return res_err(res, { id, function_name, error: e.toString() });
     }
   });
 
@@ -33,26 +28,27 @@ const function_name = "unzip";
 
 function res_ok(res, payload) {
   res.status(200).send(success(payload));
+  return success(payload);
 }
 
 function res_err(res, payload) {
   res.status(500).send(failure(payload));
+  return failure(payload);
 }
 
-function do_file_things(req) {
+function do_file_things(id, req) {
   const {
     source_bucket,
     source_filename,
     target_bucket,
     target_filename
   } = req.body.attributes;
+  console.info(`${id} ${function_name} ${source_bucket}/${source_filename} -> ${target_bucket}/${target_filename}`);
   const s_bucket = storage.bucket(source_bucket);
   const s_file = s_bucket.file(source_filename);
   const t_bucket = storage.bucket(target_bucket);
   const t_file = t_bucket.file(target_filename);
   return new Promise((res, rej) => {
-    console.log(source_bucket);
-    console.log(target_filename);
     s_file.createReadStream().pipe(gzip).pipe(t_file.createWriteStream()).on("finish", () => {
       res();
     }).on("error", err => {
