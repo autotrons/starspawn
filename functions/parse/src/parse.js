89911249fa7e20f0ae1b515a2dd183712373df71
chrisglick <chrisglick@gmail.com>
2018-05-13 21:12:48 -0600

    Added xml parsing to parser and auto loading test data

--- functions/parse/src/parse.js
+++ functions/parse/src/parse.js
@@ -1,15 +1,88 @@
-const uuid = require("uuid")
-const { failure, success } = require("@pheasantplucker/failables-node6")
-const storage = require("@google-cloud/storage")()
+const uuid = require('uuid')
+const {
+  failure,
+  success,
+  isFailure,
+  payload,
+} = require('@pheasantplucker/failables-node6')
+const storage = require('@google-cloud/storage')()
+const xml2js = require('xml2js')
+const parser = new xml2js.Parser({ explicitArray: false, trim: true })
+const {
+  createBucket,
+  bucketExists,
+  //   noUpperCase,
+  uploadFile,
+  //   getBucket,
+  //   newFile,
+  exists,
+  save,
+  getFile,
+  //   deleteFile,
+  // createWriteStream,
+  //   deleteBucket
+} = require('@pheasantplucker/gc-cloudstorage')
 
 async function parse(req, res) {
   const id = uuid.v4()
   console.log(`${id} starting`)
-  return res_ok(res, { id })
+  const r1 = getAttributes(req)
+  if (isFailure(r1)) return r1
+  const attrs = payload(r1)
+  const filePath = attrs.fileName
+
+  const r2 = await getFile(filePath)
+  if (isFailure(r2)) return r2
+  const xmlFile = payload(r2)
+
+  const r3 = await parseXmlToJson(xmlFile)
+  if (isFailure(r3)) return r3
+  const jsonJobs = payload(r3)
+
+  var options = {
+    method: 'POST',
+    uri: 'http://api.posttestserver.com/post',
+    body: {
+      some: 'payload',
+    },
+    json: true, // Automatically stringifies the body to JSON
+  }
+
+  rp(options)
+    .then(function(parsedBody) {
+      // POST succeeded...
+    })
+    .catch(function(err) {
+      // POST failed...
+    })
+
+  console.log(`jsonJobs:`, jsonJobs.root.job[0])
+
+  // return res_ok(res, { id })
 }
 
+function parseXmlToJson(xml) {
+  try {
+    let json = {}
+    const parsedToJson = parser.parseString(xml, function(err, result) {
+      json = result
+    })
+    return success(json)
+  } catch (e) {
+    return failure(e.toString())
+  }
+}
+//
+// async function sourceAtoJSON(d, authorization, replyEmail) {
+//   const r1 = await parseXmlToJson(d)
+//   const results = r1.jobs.job.map(a =>
+//     typeAToJSON(a, authorization, replyEmail)
+//   )
+//   return results
+// }
+
 function res_ok(res, payload) {
-  console.info(payload)
+  // console.info(payload)
   res.status(200).send(success(payload))
   return success(payload)
 }
@@ -20,6 +93,22 @@ function res_err(res, payload) {
   return failure(payload)
 }
 
+const getAttributes = req => {
+  try {
+    if (req.body.attributes) {
+      return success(req.body.attributes)
+    } else {
+      return failure(req, { error: 'couldnt access req.body.attributes' })
+    }
+  } catch (e) {
+    return failure(e.toString(), {
+      error: 'couldnt access req.body.attributes',
+      req: req,
+    })
+  }
+}
+
 module.exports = {
-  parse
+  parse,
+  parseXmlToJson,
 }
--- functions/parse/src/parse.test.js
+++ functions/parse/src/parse.test.js
@@ -1,32 +1,98 @@
-const { assertSuccess } = require("@pheasantplucker/failables-node6")
-const { parse } = require("./parse")
+const {
+  assertSuccess,
+  payload,
+  isFailure,
+} = require('@pheasantplucker/failables-node6')
+const { parse, parseXmlToJson } = require('./parse')
+const equal = require('assert').deepEqual
+const path = require('path')
+const fs = require('fs')
 
-describe("parse.js", function() {
+const {
+  createBucket,
+  bucketExists,
+  //   noUpperCase,
+  uploadFile,
+  //   getBucket,
+  //   newFile,
+  exists,
+  save,
+  getFile,
+  //   deleteFile,
+  // createWriteStream,
+  //   deleteBucket
+} = require('@pheasantplucker/gc-cloudstorage')
+
+const bucket = 'starspawn_tests'
+const fileName = 'chunk_output_100.xml'
+const testFileCloud = `${bucket}/${fileName}`
+const testFileRelative = `../../../samples/${fileName}`
+const testFile = path.join(__dirname, testFileRelative)
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<job>
+  <location>Fort Lauderdale, FL, United States</location>
+</job>`
+
+const json = {
+  job: {
+    location: ['Fort Lauderdale, FL, United States'],
+  },
+}
+
+const _testsetup = async () => {
+  const r1 = await bucketExists(bucket)
+  if (isFailure(r1)) return r1
+  const thisBucketExists = payload(r1)
+  if (!thisBucketExists) await createBucket(bucket)
+
+  const r2 = await exists(testFileCloud)
+  if (isFailure(r2)) return r2
+  const testFileExists = payload(r2)
+  if (!testFileExists) {
+    const upload = await uploadFile(bucket, testFile)
+  }
+}
+
+describe('parse.js', function() {
   this.timeout(540 * 1000)
-  it("should pull a batch of tags between two points in the file", async () => {
-    const input = {}
+
+  before(() => {
+    _testsetup()
+  })
+
+  it('should pull a batch of tags between two points in the file', async () => {
+    const input = { fileName: testFileCloud }
     const { req, res } = make_req_res(input)
     const result = await parse(req, res)
     assertSuccess(result)
+  })
+
+  describe(`parseXmlToJson()`, () => {
+    it(`should take XML and return a JSON obj`, () => {
+      const result = parseXmlToJson(xml)
+      assertSuccess(result)
+      const newJson = payload(result)
+      equal(newJson, json)
+    })
   })
 })
 
 function make_req_res(attributes) {
   const req = {
     body: {
-      attributes
-    }
+      attributes,
+    },
   }
   const res = {
     status: function() {
       return {
-        send: () => {}
+        send: () => {},
       }
     },
-    send: () => {}
+    send: () => {},
   }
   return {
     req,
-    res
+    res,
   }
 }
