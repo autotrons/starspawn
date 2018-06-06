6426cb4bed96f49554a0b402a19027e351f1fea6
Tim Cash <timcash@gmail.com>
2018-06-06 17:47:18 -0600

    more logging for unzip

--- services/etl/src/unzip.js
+++ services/etl/src/unzip.js
@@ -21,6 +21,7 @@ async function unzip(id, data) {
 
 async function do_file_things(id, data) {
   const { source_file, target_file } = data
+  console.info(`${id} unzip ${target_file}`)
 
   const writeStreamResult = await createWriteStream(target_file)
   if (isFailure(writeStreamResult)) return writeStreamResult
@@ -29,15 +30,21 @@ async function do_file_things(id, data) {
   const createStreamResult = await getReadStream(source_file)
   if (isFailure(createStreamResult)) return createStreamResult
   const readStream = payload(createStreamResult)
-  return unzip_it(readStream, writeStream, gzip, target_file)
+  return unzip_it(id, readStream, writeStream, gzip, target_file)
 }
 
-function unzip_it(rs, ws, gzip, target_file) {
+function unzip_it(id, rs, ws, gzip, target_file) {
   return new Promise(res => {
     rs.pipe(gzip)
       .pipe(ws)
-      .on('finish', () => res(success({ target_file })))
-      .on('error', err => res(failure(err.toString())))
+      .on('finish', () => {
+        console.info(`${id} wrote ${target_file}`)
+        res(success({ target_file }))
+      })
+      .on('error', err => {
+        console.error(`${id} unzip ${err.toString()}`)
+        res(failure(err.toString()))
+      })
   })
 }
 
