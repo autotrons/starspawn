64d679e8d07f2d85c41d8680d53063a395501d2b
chrisglick <chrisglick@gmail.com>
2018-06-20 21:20:47 -0700

    Lowered log levels away from args

--- services/etl/src/etl.js
+++ services/etl/src/etl.js
@@ -120,7 +120,7 @@ function extract_arguments(req) {
 
 async function post_command_handler(id, command, args) {
   try {
-    console.info(`${id} ${command} ${JSON.stringify(args)}`)
+    console.info(`${id} ${command}`)
     const result = await FUNCTION_MAP[command](id, args)
     if (isFailure(result)) return result
     const r2 = get_next_command(id, command, result)
