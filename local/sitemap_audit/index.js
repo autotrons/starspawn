46c44d5e4447ad6c0a127f3bdb3e650da078564e
Matt Baker <baker.mattc@gmail.com>
2018-07-27 23:28:42 -0400

    add local utility for sitemap auditing

---
+++ local/sitemap_audit/index.js
@@ -0,0 +1,41 @@
+const get = require('simple-get')
+const xmlFlow = require('xml-flow')
+
+let count = 0
+let matchFilter = 0
+
+function main() {
+  get('https://joblog.app/sitemapindex.xml', function (error, httpStream) {
+    if (error) throw error
+    if (httpStream.statusCode != 200) throw new Error(`Unexpected status code while downloading sitemap index: ${httpStream.statusCode}`)
+
+    xmlStream = xmlFlow(httpStream)
+    xmlStream.on('tag:sitemap', processSitemap)
+    xmlStream.on('end', updateOutput)
+  })
+}
+
+function processSitemap(sitemap) {
+  get(sitemap.loc, function (error, httpStream) {
+    if (error) throw error
+    if (httpStream.statusCode != 200) throw new Error(`Unexpected status code while downloading sitemap: ${httpStream.statusCode}`)
+
+    xmlStream = xmlFlow(httpStream)
+    xmlStream.on('tag:url', processJobUrl)
+  })
+}
+
+const processJobUrl = function (jobUrl) {
+  count++
+  if (jobUrl.loc.indexOf('-') === -1) matchFilter++
+
+  if(count % 10000 === 0) updateOutput()
+}
+
+function updateOutput() {
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write(`${count} urls processed, ${matchFilter} match specified filter`)
+}
+
+main()
